7268f1909746ebb0c12151739a5fbc2681b39800
Eduardo Weber <eduardo.weber@shipveho.com>
2026-03-11 10:40:04 -0300

    feat: add AWS SDK and DynamoDB dependencies for data access

--- .projenrc.ts
+++ .projenrc.ts
@@ -12,7 +12,12 @@ const project = new GaiaCdkApp({
   name: 'webhook-transformer-service',
   projenrcTs: true,
 
-  // deps: [],                                                                                                                                                                                                                                                                                      /* Runtime dependencies of this module. */
+  deps: [
+    'dynamodb-onetable',
+    '@aws-sdk/client-dynamodb',
+    'aws-xray-sdk-core',
+    'ulid',
+  ],
   // description: undefined,                                                                                                                                                                                                                                                                        /* The description is just a string that helps people understand the purpose of the package. */
   // environments: [{ name: 'dev', branch: 'dev', usedForDevelopment: true, awsAccountId: '657230704726' }, { name: 'staging', awsAccountId: '048595045497' }, { name: 'prod',  dependsOn: ['staging'], requireManualDeployApproval: true, enableCiDiffJob: true, awsAccountId: '595208618232' }],  /* Options to define application environments. */
   // packageName: undefined,                                                                                                                                                                                                                                                                        /* The "name" in package.json. */
